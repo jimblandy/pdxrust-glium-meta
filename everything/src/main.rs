2ee74590566502e52a7618d5245d27d93dc8865a
Jim Blandy <jimb@red-bean.com>
2018-06-02 14:04:35 -0700

    triangle: Simplify, clean up.

--- everything/src/main.rs
+++ everything/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 #[macro_use]
 extern crate glium;
 
@@ -12,12 +14,26 @@ fn scale(a: &[f32; 3], scale: f32) -> [f32; 3] {
     [ a[0] * scale, a[1] * scale, a[2] * scale ]
 }
 
+fn negate(a: &[f32; 3]) -> [f32; 3] {
+    scale(a, -1.0)
+}
+
 fn add(a: &[f32; 3], b: &[f32; 3]) -> [f32; 3] {
     [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 }
 
 fn subtract(a: &[f32; 3], b: &[f32; 3]) -> [f32; 3] {
-    add(a, &scale(b, -1.0))
+    add(a, &negate(b))
+}
+
+fn length(a: &[f32; 3]) -> f32 {
+    f32::sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2])
+}
+
+fn normalize(a: &[f32; 3]) -> [f32; 3] {
+    let inverse_length = 1.0 / length(a);
+    assert!(!inverse_length.is_infinite());
+    scale(a, inverse_length)
 }
 
 /// Return the point `angle` radians around the origin-centered ellipse whose
@@ -29,23 +45,17 @@ fn mix_by_angle(i: &[f32; 3], j: &[f32; 3], angle: f32) -> [f32; 3] {
 }
 
 /// Properties identifying an isosceles triangle spinning about its axis of
-/// symmetry in 3-space.
+/// symmetry in 3-space, with a distinguished front face.
 struct Triangle {
     /// Location of the triangle's tip (the corner that lies on the axis of
     /// rotation).
     tip: [f32; 3],
 
-    /// Vector from the trangle's tip to the midpoint of its base (the side
-    /// opposite the tip).
-    tip_to_base_midpt: [f32; 3],
+    /// The midpoint of the triangle's base (the side opposite the tip).
+    base_midpt: [f32; 3],
 
-    /// Vector from the midpoint of the base to one of the base corners,
-    /// in the unrotated state.
-    base_midpt_to_corner: [f32; 3],
-
-    /// Vector normal to `base_midpt` and `corner`, of the same length as
-    /// `corner`. (This could just be derived from base_midpt and corner.)
-    normal: [f32; 3],
+    /// Vector from base_midpt to the corner clockwise from the tip.
+    base_midpt_to_corner: [f32; 3]
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -58,13 +68,10 @@ implement_vertex!(Vertex, position);
 impl Triangle {
     /// Return the positions of this triangle's three corners, with the triangle
     /// rotated about its axis by `spin` radians.
-    fn corners(&self, spin: f32) -> [[f32; 3]; 3] {
-        let base_midpt = add(&self.tip, &self.tip_to_base_midpt);
-        let base_midpt_to_corner = mix_by_angle(&self.base_midpt_to_corner,
-                                                &self.normal,
-                                                spin);
-        let corner1 = add(&base_midpt, &base_midpt_to_corner);
-        let corner2 = subtract(&base_midpt, &base_midpt_to_corner);
+    fn corners(&self) -> [[f32; 3]; 3] {
+        let corner1 = add(&self.base_midpt, &self.base_midpt_to_corner);
+        let corner2 = subtract(&self.base_midpt, &self.base_midpt_to_corner);
+        // Viewed from the front, our vertices must appear in clockwise order.
         [self.tip, corner1, corner2]
     }
 }
@@ -77,7 +84,7 @@ fn main() -> Result<(), Box<Error>> {
         .with_vsync(true);
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    let triangle_interiors =
+    let triangle_interiors_program =
         Program::from_source(&display,
                              &include_str!("tri.vert"),
                              &include_str!("interior.frag"),
@@ -86,41 +93,36 @@ fn main() -> Result<(), Box<Error>> {
 
     let triangle = Triangle {
         tip: [ 0.5, 0.0, 0.0 ],
-        tip_to_base_midpt: [ -0.5, 0.0, 0.5 ],
-        base_midpt_to_corner: [ 0.0, 0.707, 0.0 ],
-        normal: [ -0.5, 0.0, -0.5 ]
+        base_midpt: [ 0.0, 0.0, -0.5 ],
+        base_midpt_to_corner: [ 0.0, -0.5, 0.0 ]
     };
 
-    loop {
+    let mut window_open = true;
+    while window_open {
         let mut frame = display.draw();
-        frame.clear_color(0.0, 0.0, 1.0, 1.0);
+        frame.clear_color(1.0, 1.0, 1.0, 1.0);
 
-        let positions = triangle.corners(0.0);
-        let vertices: Vec<_> = positions.iter()
-            .map(|&position| Vertex { position })
-            .collect();
+        let mut vertices = Vec::new();
+
+        vertices.extend(triangle.corners().iter()
+                        .map(|&position| Vertex { position }));
         assert_eq!(vertices.len(), 3);
         let vertex_buffer = VertexBuffer::new(&display, &vertices)?;
 
         frame.draw(&vertex_buffer, &glium::index::NoIndices(PrimitiveType::TrianglesList),
-                   &triangle_interiors,
+                   &triangle_interiors_program,
                    &uniform! {}, &Default::default())?;
         frame.finish()?;
-
-        let mut should_exit = false;
 
         events_loop.poll_events(|event| {
             match event {
                 // Break from the main loop when the window is closed.
                 glutin::Event::WindowEvent { event: WindowEvent::Closed, .. } => {
-                    should_exit = true;
+                    window_open = false;
                 }
                 _ => (),
             }
         });
-
-        if should_exit {
-            return Ok(());
-        }
     }
+    Ok(())
 }
